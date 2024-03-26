76fe6d2596778e630f9bef7e228751e6abd398ed
nechama <nechamy5834@gmail.com>
2024-03-27 00:12:50 +0200

    add button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,7 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
     <button @click="console.log(msg)">Update msg</button>
+    <button @click="console.log(msg)">Update msg</button>
   </div>
 </template>
 
